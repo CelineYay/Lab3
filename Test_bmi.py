75ed95c37356005a08db8172c2dfe9d79aae4f5d
CelineYay <celine.22@ichat.sp.edu.sg>
2023-05-04 11:53:43 +0800

    pytest prac

--- Test_bmi.py
+++ Test_bmi.py
@@ -1,30 +1,24 @@
-import Lab2.bmi.ca as bmi
+import Lab2.bmi as bmi
 
 print("Test Lab2 BMI")
 
 
 def test_bmi_normal_weight():
-    result = []
-    h_in = 1.5
-    w_in = 80.0
-    test_array = 0
+    h_in = 1.75
+    w_in = 57
     result = bmi.calculate_bmi(h_in, w_in)
-    assert (result == test_array)
+    assert (result == 0)
 
 
 def test_bmi_over_weight():
-    result = []
-    h_in = 1.73
-    w_in = 57
-    test_array = 1
+    h_in = 1.5
+    w_in = 80
     result = bmi.calculate_bmi(h_in, w_in)
-    assert (result == test_array)
+    assert (result == 1)
 
 
 def test_bmi_under_weight():
-    result = []
     h_in = 1.6
     w_in = 45
-    test_array = -1
     result = bmi.calculate_bmi(h_in, w_in)
-    assert (result == test_array)
+    assert (result == -1)
